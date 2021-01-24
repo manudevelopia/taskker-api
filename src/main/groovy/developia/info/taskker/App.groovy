e76cf7fc73f32d9ae771050b5c72734b8fb53e34
manudevelopia <manu@developia.info>
2021-01-24 13:21:16 +0100

    add server.port to start

--- src/main/groovy/developia/info/taskker/App.groovy
+++ src/main/groovy/developia/info/taskker/App.groovy
@@ -2,8 +2,12 @@ package developia.info.taskker
 
 import developia.info.taskker.controller.Router
 
+import static spark.Spark.port
+
 class App {
     static void main(String[] args) {
+        Optional.ofNullable(System.getProperty("server.port"))
+                .ifPresent({ portNumber -> port(Integer.valueOf(portNumber)) })
         Router.init()
     }
 }
