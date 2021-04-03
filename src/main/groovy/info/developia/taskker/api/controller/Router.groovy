0b768662817331ced951e2839d555266423f1803
manudevelopia <manu@developia.info>
2021-04-03 13:06:43 +0200

    enable CORS

--- src/main/groovy/info/developia/taskker/api/controller/Router.groovy
+++ src/main/groovy/info/developia/taskker/api/controller/Router.groovy
@@ -7,13 +7,7 @@ import org.slf4j.LoggerFactory
 
 import static info.developia.taskker.api.controller.ExceptionController.badRequestException
 import static info.developia.taskker.api.controller.ExceptionController.notFoundException
-import static spark.Spark.before
-import static spark.Spark.exception
-import static spark.Spark.get
-import static spark.Spark.patch
-import static spark.Spark.path
-import static spark.Spark.post
-import static spark.Spark.put
+import static spark.Spark.*
 
 class Router {
     private final static Logger LOG = LoggerFactory.getLogger(Router)
@@ -21,7 +15,7 @@ class Router {
     static void init() {
         final TaskController taskController = new TaskController()
 
-        before("/*", { req, res -> LOG.info("Received api call") });
+        before("/*", { req, res -> LOG.info("Received api call") })
 
         path("/api", { ->
             path("/tasks", { ->
@@ -32,6 +26,21 @@ class Router {
                 patch("/:tid", { req, res -> taskController.markDoneAs(req, res) })
             })
         })
+
+        options("/*",
+                { request, response ->
+                    String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers")
+                    if (accessControlRequestHeaders != null) {
+                        response.header("Access-Control-Allow-Headers", accessControlRequestHeaders)
+                    }
+                    String accessControlRequestMethod = request.headers("Access-Control-Request-Method")
+                    if (accessControlRequestMethod != null) {
+                        response.header("Access-Control-Allow-Methods", accessControlRequestMethod)
+                    }
+                    return "OK"
+                })
+
+        before({ request, response -> response.header("Access-Control-Allow-Origin", "*") })
 
         exception(NotFoundException, { e, req, res -> notFoundException(res, e) })
         exception(BadRequestException, { e, req, res -> badRequestException(res, e) })
